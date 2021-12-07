ab68af81a5f4002bfe0bf1700c8b8f2c43dea7d6
Faye24 <fayemarieoscobido@gmail.com>
2021-12-07 11:06:46 +0800

    MyActivity2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+  {path: 'profile', component:ProfileComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: '**', redirectTo: 'profile'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
